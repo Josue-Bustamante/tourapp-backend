bf4344e5e3608301b318b5a3469354af87872d4c
Josue-Bustamante <n00435329@upn.pe>
2026-09-08 13:11:42 -0500

    Creacion de proyecyo Backend

---
+++ src/test/java/com/example/tourapp/TourAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
